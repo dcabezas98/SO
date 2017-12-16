38bf0f844f5fbb6911289bbaad14273145f4a99a
David Cabezas <dxabezas@gmail.com>
2017-12-16 12:38:52 +0100

    Añadida mejora

--- separaBloques.c
+++ separaBloques.c
@@ -4,8 +4,8 @@
 
   Escribe en un fichero de salida llamado salida.txt el número
   de bloques y el contenido de cada bloque.
- 
 */
+
 #include<unistd.h>
 #include<stdio.h>
 #include<string.h>
@@ -23,8 +23,9 @@ int main(int argc, char *argv[])
   int n = 0;
 
   char caracter[1];
-  char bloque[50];
+  char bloque[256];
   int numChar = 0;
+  int nbytes;
 
   if( (ifd=open(argv[1],O_RDONLY))<0) {
     printf("\nError %d en open",errno);
@@ -32,28 +33,29 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
 
+  umask(0);
+  
   if( (ofd=open("salida.txt",O_CREAT|O_TRUNC|O_WRONLY,S_IRUSR|S_IWUSR))<0) {
     printf("\nError %d en open de la salida",errno);
     perror("\nError en open de la salida");
     exit(EXIT_FAILURE);
   }
 
-  lseek(ofd,strlen("El numero de bloques es "),SEEK_SET);
-  write(ofd,"          ",10);
+  nbytes = lseek(ifd, 0, SEEK_END);
+  lseek(ifd, 0, SEEK_SET);
+
+  sprintf(bloque, "El número de bloques es %d", (nbytes+80)/80);
+  write(ofd,bloque,strlen(bloque));
   
   while(read(ifd,caracter,1) > 0) {
     if(numChar%80 == 0){
       n++;
-      sprintf(bloque,"\nBloque %d\n",n);
+      sprintf(bloque,"\n\nBloque %d\n",n);
       write(ofd,bloque,strlen(bloque));
     }
     write(ofd,caracter,1);
     numChar++;
   }
-
-  sprintf(bloque,"El numero de bloques es %d",n);
-  lseek(ofd,0,SEEK_SET);
-  write(ofd,bloque,strlen(bloque));
 
   close(ifd);
   close(ofd);
